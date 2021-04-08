520677f683103e8952357a9ededde53c3106f122
160856 <160856@stud.prz.edu.pl>
2021-04-08 14:01:05 +0200

    commit

---
+++ temp.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# tekst =  "Lorem ipsum dolor sit amet, ..,"
+# print (tekst)
+
+
+# paliwo = 8.5 * (67*0.1 + 124*0.15 + 11.7*0.2)
+# autostrada = 124*0.17
+# prom = 50
+# koszt = paliwo + autostrada + prom
+# print (koszt)
+# print (koszt/2)
+# print (koszt/3)
+# print (koszt/4)
+# print (koszt/5)
+
+# jakas_zmienna = "To jest jakis napis"
+# print (jakas_zmienna)
+# jakas_zmienna = 8.5
+# print (jakas_zmienna)
+
+# lubie_placki = True
+# lista = [1,2,3,4,"napis", True, False, 8.5, 11]
+# slownik = {"jablecznik" : "ciasto z jablek", "czy lubisz jablecznik" : True}
+
+# d=[10, 8, 10, 12, 6, 8, 7, 12, 10, 16, 16, 9, 14, 9, 11, 17, 18, 9, 5, 17, 11, 17, 7, 7, 12, 9, 5, 18, 6, 7, 9, 9, 6, 8, 8, 11, 13, 16, 8, 8, 12, 5, 18, 15, 17, 18, 7, 8, 13, 5, 12, 11, 11, 12, 5, 17, 7, 15, 10, 14, 18, 5, 8, 9, 10, 14, 15, 13, 16, 14, 17, 16, 10, 7, 14, 15, 17, 11, 10, 18, 18, 9, 12, 18, 12, 13, 7, 10, 16, 12, 16, 8, 11, 15, 8, 7, 7, 10, 13, 13]
+# print (d[16])
+
+# print (d[9])
+# print (d[10])
+# print (d[11])
+# print (d[12])
+# print (d[13])
+# print (d[14])
+
+# print (d[9:15])
+# print (d[:5])
+# print (d[-1])
+# d[11] = d[11] - 0.1
+
+# d.append(14)
+# print (d)
+
+# lista = range(15)
+# print (lista)
+
+# for i in range(len(d)):
+#     d[i] = d[i]*0.1
+    
+# print (d)
+
+# for element in [1,2,3,4,5]:
+#   print (element)
+# print ("zrobimy sobie odstep")
+# print (element)
+
+# zmienna = 8
+# if zmienna > 5 :
+#     print("Zmienna jest wieksza niz 5")
+
+# zmienna = 8
+# if zmienna == 15:
+#     print ("Zmienna jest rowna 15")
+# else:
+#     print ("Zmienna nie jest rowna 15")
+
+# zmienna = 8
+# if zmienna == 1:
+#     print ("Wartosc zmiennej to jeden")
+# else:
+#     if zmienna == 2:
+#         print ("Wartosc zmiennej to dwa")
+#     else:
+#         print ("Wartosc zmiennej nie jest ani jeden ani dwa")
+
+# x=0
+# while x<5:
+#     print ("zmienna x wynosi ", x)
+#     x+=1
+    
+# x=0
+# while True:
+#     print ("To wyswietli sie zawsze gdy warunek jest spelniony")
+#     if x==5:
+#         print ("zmienna x wynosi ", x)
+#         break
+#     else:
+#         print (x)
+#     x+=1 
+
+# def f(x):
+#     return (2* (x**3))/8.51
+
+# print (f(5))
+
+
+# def f(x=0):
+#     return (2 * (x**3))/8.51
+ 
+# print (f())
+# print (f(5))
